6034575f7504ee5725a77ac5f584fd0dd8facc64
MinJun2 <kaf829@naver.com>
2025-06-04 08:55:17 +0900

    0603 머신러닝 학습

--- pre_study.py
+++ pre_study.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metics import accuracy_score
+from sklearn.metrics import accuracy_score
 
 A = np.array([[1,2,3,],[4,5,6]])
 B = np.array([[7,8],[9,10],[11,12]])
