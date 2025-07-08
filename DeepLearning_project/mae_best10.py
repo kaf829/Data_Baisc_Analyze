4cb2fce1024282d043a771d5da554a6a5bbefb1e
MinJun2 <kaf829@naver.com>
2025-07-08 09:04:23 +0900

    7월 7일 학습

--- DeepLearning_project/mae_best10.py
+++ DeepLearning_project/mae_best10.py
@@ -7,7 +7,7 @@ file_path = "./모델_학습결과3.csv"
 df = pd.read_csv(file_path)
 
 # 모델별로 MAE가 낮은 상위 10개씩 추출
-top10_by_model = df.sort_values(by='MAE').groupby('model').head(10)
+top10_by_model = df.sort_values(by='MAE').groupby('jupyjupymodel').head(10)
 
 # 저장
 output_path = "./top10_lowest_mae_by_model.csv"
